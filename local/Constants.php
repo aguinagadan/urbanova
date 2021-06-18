6233929aef5c53cbe61faff6190b4aedf83bbd2c
Samuel Rojas Pachas <samuel.rojas@globant.com>
2021-06-17 22:53:17 -0500

    plugin task ad users

---
+++ local/Constants.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace moodleconstants;
+
+class Constants {
+
+	const AD_URL = 'estrategicaperu.onmicrosoft.com';
+	const AD_CLIENT_ID = '15694cd7-31c2-4b7c-acf8-f257c754d499';
+	const AD_CLIENT_SECRET = '30rhja.scIjTqcv_.~61-M2gSrzbvO71Z9';
+
+}
---
+++ local/adusers/version.php
@@ -0,0 +1,8 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die;
+$plugin->requires   = 2021000000;                       // This plugin requires Moodle VER 3.0.
+$plugin->version    = 2021000000;                       // This plugins version number.
+$plugin->release    = 'v1.0';                           // This plugins release number.
+$plugin->maturity   = MATURITY_STABLE;
+$plugin->component  = 'local_adusers';

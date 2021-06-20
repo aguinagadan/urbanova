be3281ea91eed72f0d2ed0d929d8a128d22032f8
Samuel Rojas Pachas <samuel.rojas@globant.com>
2021-06-20 14:02:54 -0500

    test

--- local/adusers/adusers_test.php
+++ local/adusers/adusers_test.php
@@ -7,6 +7,49 @@ const AD_URL = 'estrategicaperu.onmicrosoft.com';
 const AD_CLIENT_ID = '15694cd7-31c2-4b7c-acf8-f257c754d499';
 const AD_CLIENT_SECRET = '30rhja.scIjTqcv_.~61-M2gSrzbvO71Z9';
 
+// Set params
+$client_id = '15694cd7-31c2-4b7c-acf8-f257c754d499'; // Client App ID (from Microsoft)
+$client_secret = '30rhja.scIjTqcv_.~61-M2gSrzbvO71Z9';; // Client App Secrect (from Microsoft)
+$client_space = 'estrategicaperu.onmicrosoft.com'; // Microsoft Domain ID
+$client_redirect = "https://www.campusqroma.com/local/systemscripts/testSSO.php"; // Where to redirect after authenticated
+$scopes = "User.ReadAll"; // Scopes of data requested (ex. 'openid+profile+user.read')
+// Get Auth Code from MS API
+
+	// Build API Token Url
+	$url = "https://login.microsoftonline.com/" . $client_space . "/oauth2/v2.0/token";
+	// Url passed parameters
+	$fields = array(
+		"client_id" => $client_id,
+		"client_secret" => $client_secret,
+		"scope" => $scopes,
+		"grant_type" => "client_credentials"
+	);
+
+	// For each API Url field
+	foreach($fields as $key=>$value) {
+		$fields_string .= $key . "=" . $value . "&";
+	}
+	// Trim, prep query string
+	rtrim($fields_string, "&");
+
+	// Make HTTP request
+	$ch = curl_init();
+	curl_setopt($ch,CURLOPT_URL, $url);
+	curl_setopt($ch,CURLOPT_HTTPHEADER, array("Content-Type: application/x-www-form-urlencoded"));
+	curl_setopt($ch,CURLOPT_POST, count($fields));
+	curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+	$result = curl_exec($ch);
+	$result = json_decode($result);
+	curl_close($ch);
+
+	var_dump($result->access_token);
+	exit;
+
+/*const AD_URL = 'estrategicaperu.onmicrosoft.com';
+const AD_CLIENT_ID = '15694cd7-31c2-4b7c-acf8-f257c754d499';
+const AD_CLIENT_SECRET = '30rhja.scIjTqcv_.~61-M2gSrzbvO71Z9';
+
 function execCurl2($data) {
 	$curl = curl_init();
 
@@ -62,3 +105,4 @@ var_dump($responseData);
 exit;
 
 return 'eyJ0eXAiOiJKV1QiLCJub25jZSI6ImFrWlFkeF9kYm1nT012dmdMRU4tWmdya3BSS2owVXRmNEptOTdZNUZZZ2ciLCJhbGciOiJSUzI1NiIsIng1dCI6Im5PbzNaRHJPRFhFSzFqS1doWHNsSFJfS1hFZyIsImtpZCI6Im5PbzNaRHJPRFhFSzFqS1doWHNsSFJfS1hFZyJ9.eyJhdWQiOiJodHRwczovL2dyYXBoLm1pY3Jvc29mdC5jb20iLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC8zZTAyODljMi1lMzUxLTRjNzktOTk4Yi04MTY3YzgzYTA2ZDMvIiwiaWF0IjoxNjI0MjE0MzE5LCJuYmYiOjE2MjQyMTQzMTksImV4cCI6MTYyNDIxODIxOSwiYWlvIjoiRTJaZ1lIaC9YSjVySStlbHJYZm1DajZOWXJ6TkJ3QT0iLCJhcHBfZGlzcGxheW5hbWUiOiJBdWxhIFZpcnR1YWwgVXJiYW5vdmEiLCJhcHBpZCI6IjE1Njk0Y2Q3LTMxYzItNGI3Yy1hY2Y4LWYyNTdjNzU0ZDQ5OSIsImFwcGlkYWNyIjoiMSIsImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzNlMDI4OWMyLWUzNTEtNGM3OS05OThiLTgxNjdjODNhMDZkMy8iLCJpZHR5cCI6ImFwcCIsIm9pZCI6IjVjNTY5Y2E1LTEwOWQtNDI1Zi04MzZmLTRjMzUyMDRkOTQ3YyIsInJoIjoiMC5BUVlBd29rQ1BsSGplVXlaaTRGbnlEb0cwOWRNYVJYQ01YeExyUGp5VjhkVTFKa0dBQUEuIiwicm9sZXMiOlsiRGlyZWN0b3J5LlJlYWQuQWxsIiwiVXNlci5SZWFkLkFsbCJdLCJzdWIiOiI1YzU2OWNhNS0xMDlkLTQyNWYtODM2Zi00YzM1MjA0ZDk0N2MiLCJ0ZW5hbnRfcmVnaW9uX3Njb3BlIjoiU0EiLCJ0aWQiOiIzZTAyODljMi1lMzUxLTRjNzktOTk4Yi04MTY3YzgzYTA2ZDMiLCJ1dGkiOiJGME5FN2ZrOU5FTzRRSVhlbTJvRkFRIiwidmVyIjoiMS4wIiwid2lkcyI6WyIwOTk3YTFkMC0wZDFkLTRhY2ItYjQwOC1kNWNhNzMxMjFlOTAiXSwieG1zX3RjZHQiOjEzNjAxMTAxNzd9.Qs-fHa_fwMJOMwbxPa5Dc33ZgDv9Ynp1EijOQ3DcgEGGQq1z4K7uiqdopBt-N9nvcaAAGrxCVmyK-uZnS6bn5jDX0JarhDEwFZCgxYNzWWmwf2PJLyOpE6Kmo7e25HVcIbNTnMGXih_A2rwOJ6U3oapxsXepxA9lGTw2M6HdzN47_huUkFDZf1G5MlwPsPu5BJJO7hTHAy81Xuj7xsLGy2nFY3bx7SSUu2mboI8SX-7hjNd0RjBEUhhHeOHTExEPlPQwOYVHJzyh2IQr1yiK8zy-3pmRfaKjXmt79xqKLh0GZveZC_d7fTfFvLMpDHVg_Y6ZOeUBmhwZ9jPAEyRuBw';
+*/

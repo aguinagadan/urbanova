f3dac893a8bf998c2bb8f8a540c18b815cacfb44
Samuel Rojas Pachas <samuel.rojas@globant.com>
2021-06-20 18:56:21 -0500

    custom catalogo

--- catalogo/index.php
+++ catalogo/index.php
@@ -1,16 +1,16 @@
 <?php
 global $CFG, $PAGE, $OUTPUT, $USER;
 
-require_once(dirname(__FILE__) . '/../config.php');
+require_once($CFG->dirroot .'/config.php');
 
 require_login();
 $context = context_user::instance($USER->id);
 $PAGE->set_context($context);
 
 $title = 'Catálogo de Cursos';
-$url = new moodle_url("/catalogo/index.php");
+$url = '/catalogo/index.php';
 $PAGE->set_url($url);
-$PAGE->set_pagelayout('frontpage');
+$PAGE->set_pagetype('site-index');
 $PAGE->set_title($title);
 
 echo $OUTPUT->header();

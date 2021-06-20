1ab7d0890e5a6e30bafdd3154feafd1803a17699
Samuel Rojas Pachas <samuel.rojas@globant.com>
2021-06-20 15:38:49 -0500

    test

--- local/Constants.php
+++ local/Constants.php
@@ -6,6 +6,6 @@ class Constants {
 	const AD_CLIENT_ID = '15694cd7-31c2-4b7c-acf8-f257c754d499';
 	const AD_CLIENT_SECRET = '30rhja.scIjTqcv_.~61-M2gSrzbvO71Z9';
 	const SCOPE = 'https://graph.microsoft.com/.default';
-	const GRANT_TYPE_CLIENT_CREDENTIALS = '"client_credentials';
+	const GRANT_TYPE_CLIENT_CREDENTIALS = 'client_credentials';
 
 }

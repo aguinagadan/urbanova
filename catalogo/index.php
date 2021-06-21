31d26adbc8de6f3b30c797ddcc7a8b4642f1a7de
Samuel Rojas Pachas <samuel.rojas@globant.com>
2021-06-20 19:56:36 -0500

    custom front

--- catalogo/index.php
+++ catalogo/index.php
@@ -2,7 +2,7 @@
 require_once(__DIR__ . '/../config.php');
 
 $urlparams = array();
-$PAGE->set_url('/catalogo/index.php', $urlparams);
+$PAGE->set_url('/catalogo/', $urlparams);
 
 $PAGE->set_pagetype('site-catalogo');
 $PAGE->set_title($SITE->fullname);

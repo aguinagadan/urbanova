c566736d59428ff8a089e297f1d59c220d62e4d5
Samuel Rojas Pachas <samuel.rojas@globant.com>
2021-06-20 19:06:46 -0500

    custom front

--- catalogo/index.php
+++ catalogo/index.php
@@ -1,37 +1,10 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-/**
- * Moodle frontpage.
- *
- * @package    core
- * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
-
 require_once(__DIR__ . '/../config.php');
 
 $urlparams = array();
 $PAGE->set_url('/catalogo/index.php', $urlparams);
-//$PAGE->set_pagelayout('frontpage');
 
-$PAGE->set_pagetype('site-index');
-$PAGE->set_docs_path('');
-$editing = $PAGE->user_is_editing();
+$PAGE->set_pagetype('site-catalogo');
 $PAGE->set_title($SITE->fullname);
 $PAGE->set_heading($SITE->fullname);
 echo $OUTPUT->header();

6fc04fc342da2c8d97052d6509ebba5cc83f55ca
Samuel Rojas Pachas <samuel.rojas@globant.com>
2021-06-23 19:59:16 -0500

    frontpage and css

--- index.php
+++ index.php
@@ -96,6 +96,7 @@ $PAGE->set_docs_path('');
 $editing = $PAGE->user_is_editing();
 $PAGE->set_title($SITE->fullname);
 //$PAGE->set_heading($SITE->fullname);
+$PAGE->requires->css('/local/customfront/css/grid.css');
 $courserenderer = $PAGE->get_renderer('core', 'course');
 echo $OUTPUT->header();
 

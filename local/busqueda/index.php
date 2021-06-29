9824840099b172d866a3c3ae6582a3d7653e613b
Samuel Rojas Pachas <samuel.rojas@globant.com>
2021-06-29 00:06:49 -0500

    add users, busqueda

---
+++ local/busqueda/index.php
@@ -0,0 +1,12 @@
+<?php
+require_once(__DIR__ . '/../config.php');
+
+require_login();
+
+$urlparams = array();
+$PAGE->set_url('/local/busqueda/', $urlparams);
+
+$PAGE->set_title($SITE->fullname);
+echo $OUTPUT->header();
+include('home.html');
+echo $OUTPUT->footer();

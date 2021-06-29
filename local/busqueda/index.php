b5f063cc9af37775dd88b0a5a362d8b5394a6358
Samuel Rojas Pachas <samuel.rojas@globant.com>
2021-06-29 00:08:57 -0500

    add users

--- local/busqueda/index.php
+++ local/busqueda/index.php
@@ -4,7 +4,7 @@ require_once(__DIR__ . '/../config.php');
 require_login();
 
 $urlparams = array();
-$PAGE->set_url('/local/busqueda/', $urlparams);
+$PAGE->set_url('/local/busqueda/index.php', $urlparams);
 
 $PAGE->set_title($SITE->fullname);
 echo $OUTPUT->header();

4eeb30cc98631ed59c620d4a04604b60c480b22d
Samuel Rojas Pachas <samuel.rojas@globant.com>
2021-07-10 17:05:16 -0500

    comments 1

--- index.php
+++ index.php
@@ -98,6 +98,8 @@ $PAGE->set_title($SITE->fullname);
 //$PAGE->set_heading($SITE->fullname);
 $PAGE->requires->css('/local/customfront/css/grid.css');
 $PAGE->requires->css('/local/customfront/css/style.css');
+$PAGE->requires->css('/local/customfront/css/catalogo.css');
+$PAGE->requires->css('/catalogo/css/index.css');
 $courserenderer = $PAGE->get_renderer('core', 'course');
 echo $OUTPUT->header();
 

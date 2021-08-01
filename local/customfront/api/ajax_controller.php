bfbd911b224ccacbd64cc57410441123e2d2a07b
Samuel Rojas Pachas <samuel.rojas@globant.com>
2021-08-01 13:10:52 -0500

    updates

--- local/customfront/api/ajax_controller.php
+++ local/customfront/api/ajax_controller.php
@@ -6,7 +6,6 @@ use core_course\external\course_summary_exporter;
 error_reporting(E_ALL);
 require_once(dirname(__FILE__) . '/../../../config.php');
 require_once($CFG->dirroot . '/enrol/externallib.php');
-require_once($CFG->dirroot. '/course/lib.php');
 
 try {
 	global $USER, $PAGE;

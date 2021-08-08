84f65ea2d7f657bdc81626b2dfc063271e134a2a
Samuel Rojas Pachas <samuel.rojas@globant.com>
2021-08-08 04:23:11 -0500

    recordatorio updates

--- local/recordatorio/testrecordatorio.php
+++ local/recordatorio/testrecordatorio.php
@@ -18,6 +18,13 @@ function enviarRecordatorios($courseId) {
 	foreach($users as $user) {
 		$foruser = core_user::get_user($user->id);
 		$message = 'Por favor no se olvide de completar su curso pendiente';
+
+		var_dump($foruser);
+		var_dump($supportuser);
+		var_dump($subject);
+		var_dump($message);
+		exit;
+
 		email_to_user($foruser, $supportuser, $subject, $message);
 	}
 }

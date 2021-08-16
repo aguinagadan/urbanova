036ba630ee1af47c9aa408f610ac81cc07cb4721
Samuel Rojas Pachas <samuel.rojas@globant.com>
2021-08-16 16:09:49 -0500

    add test for photos

--- local/adusers/adusers_photo_test.php
+++ local/adusers/adusers_photo_test.php
@@ -3,7 +3,7 @@ require_once(dirname(__FILE__) . '/../../config.php');
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-global $USER, $OUTPUT;
+global $USER, $OUTPUT, $PAGE;
 
 
 function execCurlPhoto($data) {

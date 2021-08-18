ae567c4fa1060bcb5ea8aec7d366cf2a507d6919
Samuel Rojas Pachas <samuel.rojas@globant.com>
2021-08-17 22:41:13 -0500

    updates

--- matricula/js/main.js
+++ matricula/js/main.js
@@ -28,6 +28,7 @@ $(document).ready(function () {
             var participantes = $("#participantes");
             participantes.empty();
             participantes.append('<option value="0">Selecciona a los participantes</option>');
+            participantes.append('<option value="all">Matricular a todos</option>');
             for (var i=0; i<data.data.length; i++) {
                 participantes.append('<option value="' + data.data[i] + '">' + data.data[i] + '</option>');
             }
